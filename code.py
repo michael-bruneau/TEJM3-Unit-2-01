afa5a32164097b68deeb9dbc8ef5f0a188392e1b
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2025-02-11 22:18:37 +0000

    python code

---
+++ code.py
@@ -0,0 +1,16 @@
+# This program uses python
+
+import board
+import digitalio
+import time
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True
+    time.sleep(1)
+
+    led.value = False
+    time.sleep(1)
+    
